7bd77be4787be980d395e63ca79e0ff1afe62437
Gomezgani Mlowoka <154627682+gomezganimlowoka@users.noreply.github.com>
2026-04-15 14:34:37 +0000

    configure email still

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ class Config:
     MAIL_SERVER = 'smtp.gmail.com'       # or your SMTP provider
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'your@gmail.com'     # sender email
-    MAIL_PASSWORD = 'your_app_password'  # use an App Password, not your real one
-    MAIL_DEFAULT_SENDER = 'your@gmail.com'
-    CONTACT_MAIL_RECIPIENT = 'your@gmail.com'  # where to receive messages
+    MAIL_USERNAME = 'gomezganimlowoka95@gmail.com'     # sender email
+    MAIL_PASSWORD = '2001g1998w'  # use an App Password, not your real one
+    MAIL_DEFAULT_SENDER = 'gomezganimlowoka95@gmail.com'
+    CONTACT_MAIL_RECIPIENT = 'gomezganimlowoka95@gmail.com'  # where to receive messages
