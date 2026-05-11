aaedb22e3a4fd0a2f0f0f4f192a43c78c85f3b4e
Gomezgani Mlowoka <154627682+gomezganimlowoka@users.noreply.github.com>
2026-05-11 19:38:22 +0000

    now the emailling function works ok

--- app/static/js/sendEmail.js
+++ app/static/js/sendEmail.js
@@ -1,9 +1,10 @@
 function sendEmail() {
     const templateParams = {
-        from_name: document.getElementById("fn1").value,
+        from_name: document.getElementById("fn1").value + " " + document.getElementById("ln1").value,
         from_email: document.getElementById("em1").value,
-        message: document.getElementById("ms").value
+        message: document.getElementById("msg1").value  
     };
+
     emailjs.send('service_fevplkl', 'template_eq71hhl', templateParams)
         .then(function(response) {
             alert('Message sent successfully!');
@@ -11,3 +12,9 @@ function sendEmail() {
             alert('Failed to send message. Please try again later.');
         });
 }
+
+// Attach to form submit
+document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    sendEmail();
+});
