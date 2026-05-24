709efb6b4ddef58ead56866136e5f6d4b1485f90
Gomezgani Mlowoka <gomezganimlowoka95@gmail.com>
2026-05-24 17:41:03 +0200

    add success message and error message for the contact form

--- app/static/js/sendEmail.js
+++ app/static/js/sendEmail.js
@@ -5,12 +5,19 @@ function sendEmail() {
         message: document.getElementById("msg1").value  
     };
 
-    emailjs.send('service_fevplkl', 'template_eq71hhl', templateParams)
-        .then(function(response) {
-            alert('Message sent successfully!');
-        }, function(error) {
-            alert('Failed to send message. Please try again later.');
-        });
+   emailjs.send('service_fevplkl', 'template_eq71hhl', templateParams)
+    .then(function(response) {
+        document.getElementById("success-msg").classList.remove("d-none");
+        setTimeout(function() {
+            document.getElementById("success-msg").classList.add("d-none");
+        }, 5000);
+    }, function(error) {
+        document.getElementById("error-msg").classList.remove("d-none");
+        setTimeout(function() {
+            document.getElementById("error-msg").classList.add("d-none");
+        }, 5000);
+    });
+
 }
 
 // Attach to form submit
@@ -18,3 +25,12 @@ document.querySelector("form").addEventListener("submit", function(e) {
     e.preventDefault();
     sendEmail();
 });
+
+function onSubmit() {
+    const btn = document.getElementById("btn-send");
+    btn.innerText = "Sending...";
+    btn.disabled = true;
+
+    // Clears all fields at once instead of one by one
+    document.querySelector("form").reset();
+}
